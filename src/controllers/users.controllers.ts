e57bc3608406479c92f093f5bea8fd43b11fe213
Thuyet Le <ledangthuyet@gmail.com>
2023-10-01 18:13:00 +0700

    Update ReqBody Interface

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
-import {ParamsDictionary} from 'express-serve-static-core'
-
+import { ParamsDictionary } from 'express-serve-static-core'
 
 import userService from '~/services/users.services'
-import { LogoutReqBody, RegisterReqBody, TokenPayload } from '~/models/request/user.request'
-import { error } from 'node:console'
+import {
+  LoginReqBody,
+  LogoutReqBody,
+  RegisterReqBody,
+  TokenPayload,
+  VerifyEmailReqBody
+} from '~/models/request/user.request'
 import { ObjectId } from 'mongodb'
 import User from '~/models/schemas/User.schema'
 import USERS_MESSAGES from '~/constants/messsage'
@@ -13,67 +17,75 @@ import HTTP_STATUS from '~/constants/httpStatus'
 import { UserVerifyStatus } from '~/constants/enum'
 
 // Login Controller
-export const loginController = async (req: Request, res: Response) => {
-  const user  = req.user as User
+export const loginController = async (req: Request<ParamsDictionary, any, LoginReqBody>, res: Response) => {
+  const user = req.user as User
   const user_id = user._id as ObjectId
   const result = await userService.login(user_id.toString())
   return res.json({
     message: USERS_MESSAGES.LOGIN_SUCCESS,
-    result 
+    result
   })
 }
 
 // Register Controller
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response, next: NextFunction) => {  
-    const result = await userService.register( req.body )   
-    return res.json({
-      message: USERS_MESSAGES.REGISTER_SUCCESS,
-      result
-    })  
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await userService.register(req.body)
+  return res.json({
+    message: USERS_MESSAGES.REGISTER_SUCCESS,
+    result
+  })
 }
- 
+
 // Logout Controller
 
-export const logoutController = async(req: Request<ParamsDictionary, any, LogoutReqBody>, res: Response) => {
-  const {refresh_token} = req.body
+export const logoutController = async (req: Request<ParamsDictionary, any, LogoutReqBody>, res: Response) => {
+  const { refresh_token } = req.body
   const result = await userService.logout(refresh_token)
   return res.json(result)
- }
+}
 
 // VerifyEmail Controller
 
- export const emailVerifyController = async ( req: Request, res: Response, next: NextFunction) => {
-   const {user_id} = req.decoded_email_verify_token as TokenPayload
-   const user = await databaseService.users.findOne({
+export const verifyEmailController = async (
+  req: Request<ParamsDictionary, any, VerifyEmailReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { user_id } = req.decoded_email_verify_token as TokenPayload
+  const user = await databaseService.users.findOne({
     _id: new ObjectId(user_id)
-   })
-   // Nếu ko tìm thấy user thì sẽ báo lỗi 
+  })
+  // Nếu ko tìm thấy user thì sẽ báo lỗi
 
-   if (!user) {
+  if (!user) {
     return res.status(HTTP_STATUS.NOT_FOUND).json({
-      message: USERS_MESSAGES.USER_NOT_FOUND,
+      message: USERS_MESSAGES.USER_NOT_FOUND
     })
-   }
-   /// Sau khi da verify thi token === '' 
-   /// Se tra về status OK với message là đã verify trước đó rồi
-   if ( user.email_verify_token === '') {
+  }
+  /// Sau khi da verify thi token === ''
+  /// Se tra về status OK với message là đã verify trước đó rồi
+  if (user.email_verify_token === '') {
     return res.json({
       message: USERS_MESSAGES.EMAIL_ALREADY_VERIFIED_BEFORE
     })
-   }
-   //Nếu chưa verify thì sẽ gọi tới userservice
-   const result = await userService.verifyEmail(user_id)
-   return res.json({
+  }
+  //Nếu chưa verify thì sẽ gọi tới userservice
+  const result = await userService.verifyEmail(user_id)
+  return res.json({
     message: USERS_MESSAGES.EMAIL_VERIFY_SUCCESS,
     result
-   })
- }
+  })
+}
 
- // Resend VerifyEmail Controller
+// Resend VerifyEmail Controller
 
- export const resendVerifyEmailController = async ( req: Request, res: Response, next: NextFunction) => {
-  const {user_id} = req.decoded_authorization as TokenPayload
-  const user = await databaseService.users.findOne({_id: new ObjectId(user_id)})
+export const resendVerifyEmailController = async (req: Request, res: Response, next: NextFunction) => {
+  const { user_id } = req.decoded_authorization as TokenPayload
+  const user = await databaseService.users.findOne({ _id: new ObjectId(user_id) })
   // Neu user ko ton tai
   if (!user) {
     return res.status(HTTP_STATUS.NOT_FOUND).json({
@@ -81,12 +93,12 @@ export const logoutController = async(req: Request<ParamsDictionary, any, Logout
     })
   }
   // User da verify chua
-   if( user.verify === UserVerifyStatus.Verified) {
+  if (user.verify === UserVerifyStatus.Verified) {
     return res.json({
       message: USERS_MESSAGES.EMAIL_ALREADY_VERIFIED_BEFORE
     })
-   }
-   // Neu chua verify thi goi toi service de gui lai verifytoken va tra ve thong bao da gui lai emailverify
-   const result = await userService.resendVerifyEmail(user_id)
-   return res.json(result)
- }
+  }
+  // Neu chua verify thi goi toi service de gui lai verifytoken va tra ve thong bao da gui lai emailverify
+  const result = await userService.resendVerifyEmail(user_id)
+  return res.json(result)
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,17 +1,27 @@
-
-import {Router} from 'express'
-const userRouter = Router();
-import { emailVerifyController, loginController, logoutController, registerController, resendVerifyEmailController } from '~/controllers/users.controllers';
-import { RefreshTokenValidator, accessTokenValidator, emailVerifyTokenValidator, loginValidator, registerValidator } from '~/middlewares/users.middlewares';
-import { wrapRequestHandler } from '~/utils/handlers';
+import { Router } from 'express'
+const userRouter = Router()
+import {
+  verifyEmailController,
+  loginController,
+  logoutController,
+  registerController,
+  resendVerifyEmailController
+} from '~/controllers/users.controllers'
+import {
+  RefreshTokenValidator,
+  accessTokenValidator,
+  emailVerifyTokenValidator,
+  loginValidator,
+  registerValidator
+} from '~/middlewares/users.middlewares'
+import { wrapRequestHandler } from '~/utils/handlers'
 
 /**
  * Des : Login a user
  * Path : /login
  * Method: POST
- * Body:  {email: string, password: string} * 
+ * Body:  {email: string, password: string} *
  */
-
 
 // Route Login
 userRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
@@ -21,36 +31,33 @@ userRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
  * Path : /login
  * Method: POST
  * Header : {Authorization: Bearer <access_token>}
- * Body:  {refresh_token: string}  
+ * Body:  {refresh_token: string}
  */
 
-
-//Route Logout 
-userRouter.post('/logout',accessTokenValidator, RefreshTokenValidator, wrapRequestHandler(logoutController))
+//Route Logout
+userRouter.post('/logout', accessTokenValidator, RefreshTokenValidator, wrapRequestHandler(logoutController))
 
 // Method Register + Validation
-userRouter.post('/register',registerValidator
-, wrapRequestHandler(registerController))
-
+userRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
 
 //Route Verify Email
 /**
  * Des : Verify email when user client click on the link in email
  * Path : /verify-email
  * Method: POST
- * Body:  {email_verify_token: string}  
+ * Body:  {email_verify_token: string}
  */
 
-userRouter.post('/verify-email',emailVerifyTokenValidator, wrapRequestHandler(emailVerifyController))
+userRouter.post('/verify-email', emailVerifyTokenValidator, wrapRequestHandler(verifyEmailController))
 
 /**
  * Des : Resend Verify email when click on the link in email
  * Path : /resend-verify-email
  * Method: POST
  * Header : {Authorization: Bearer <access_token>}
- * Body:  {}  
+ * Body:  {}
  */
 
-userRouter.post('/resend-verify-email',accessTokenValidator, wrapRequestHandler(resendVerifyEmailController))
+userRouter.post('/resend-verify-email', accessTokenValidator, wrapRequestHandler(resendVerifyEmailController))
 
-export default userRouter   
+export default userRouter
