c423361aa2a5b29f0f87decb5f30fb7c40c4d327
Thuyet Le <ledangthuyet@gmail.com>
2023-10-07 22:37:14 +0700

    Filter Image Update

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,23 +1,10 @@
-import { error } from 'console'
 import { NextFunction, Request, Response } from 'express'
-import path from 'path'
-import formidable from 'formidable'
+import { handleUploadSingleImage } from '~/utils/file'
+import { IncomingMessage } from 'http'
 
-export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024
+export const uploadSingleImageController = async (req: Request & IncomingMessage, res: Response, next: NextFunction) => {
+  const data = await handleUploadSingleImage(req as any)
+  return res.json({
+    result: data
   })
-  
-  form.parse(req, (err, fields, files) => {
-    if(err) {
-        throw err
-    }
-    res.json({
-        message: 'Upload image successfully'     
-    })
-  })  
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,11 +1,44 @@
-import fs from 'fs'
-import path from 'path'
+import fs, { readlink } from 'fs'
+import { IncomingMessage } from 'http'
+import { reject } from 'lodash'
+import path, { resolve } from 'path'
 
 export const initFolder = () => {
-    const uploadFolderPath = path.resolve('uploads/images')
-    if (!fs.existsSync(uploadFolderPath)) {
-        fs.mkdirSync(uploadFolderPath, {
-            recursive: true //! mục đích để tạo folder nested
-        })
+  const uploadFolderPath = path.resolve('uploads/images')
+  if (!fs.existsSync(uploadFolderPath)) {
+    fs.mkdirSync(uploadFolderPath, {
+      recursive: true //! mục đích để tạo folder nested
+    })
+  }
+}
+
+// TODO Hàm xử lý upload ảnh :
+export const handleUploadSingleImage = async (req: Request & IncomingMessage) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 3000 * 1024,
+    filter: function ({name, originalFilename, mimetype}) {
+        //? Neu valid có name là image và mimetype là kiểu ảnh ( luôn trả về là true hoặc false)
+        const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+        if (!valid) {
+            form.emit('error' as any, new Error('File type is not valid') as any)            
+        }
+        return valid
     }
+
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
 }
